d14113670ea86f820456d35a69f02cf4fc604e0c
Dilan Raul Martinez Hernandez <dilan_1321124027@uptecamac.edu.mx>
2024-10-16 17:03:56 -0500

    first commit

---
+++ Operaciones/Controllers/OperacionesController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.UI.WebControls;
+
+namespace Operaciones.Controllers
+{
+    public class OperacionesController : ApiController
+    {
+        [ActionName("CalculoArea")]
+        [HttpGet]
+        public double Circulo(double radio)
+        {
+            double pi = 3.1416;
+            double resultado = pi * (radio * radio);
+            return resultado;
+        }
+
+
+        [ActionName("AreaTriangulo")]
+        [HttpGet]
+        public double Triangulo(double basecerru, double altura)
+        {
+            double resultado = (basecerru * altura) / 2;
+            return resultado;
+        }
+        [ActionName("CalculoHipotenusa")]
+        [HttpGet]
+        public double Pitagoras(double catetoAdy, double catetoOpues)
+        {
+            double hipotenusa = (catetoAdy * catetoAdy) + (catetoOpues * catetoOpues);
+            return Math.Sqrt(hipotenusa);
+        }
+    }
+}
